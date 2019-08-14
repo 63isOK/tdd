7ace5474e1b15e65a2934124bf8cee0e99bd2b72
63isOK <1876180681@qq.com>
2019-08-14 01:06:22 +0000

    新需求-未指定具体某人时,打印hello world 红灯

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,9 +3,20 @@ package main
 import "testing"
 
 func TestHelloWorld(t *testing.T) {
-	got := HelloWorld("Jim")
-	want := "hello Jim"
-	if got != want {
-		t.Errorf("want '%s' got '%s'", want, got)
-	}
+
+	t.Run("say hello to someone", func(t *testing.T) {
+		got := HelloWorld("Jim")
+		want := "hello Jim"
+		if got != want {
+			t.Errorf("want '%s' got '%s'", want, got)
+		}
+	})
+
+	t.Run("say hello world when there is nobody", func(t *testing.T) {
+		got := HelloWorld("")
+		want := "hello world"
+		if got != want {
+			t.Errorf("want '%s' got '%s'", want, got)
+		}
+	})
 }
