91f63421add87ce0339d7464dd481c4cfbd85da6
63isOK <1876180681@qq.com>
2019-08-14 09:13:05 +0000

    计算切片的值 红灯 绿灯 重构

--- sum/sum.go
+++ sum/sum.go
@@ -13,6 +13,15 @@ func Sum(n [5]int) (sum int) {
 	return
 }
 
+// SumSlice calc the sum of slice
+func SumSlice(n []int) (sum int) {
+	for _, x := range n {
+		sum += x
+	}
+
+	return
+}
+
 func main() {
 	s := Sum([5]int{1, 1, 1, 1, 1})
 	fmt.Println(s)
--- sum/sum_test.go
+++ sum/sum_test.go
@@ -11,3 +11,28 @@ func TestSum(t *testing.T) {
 		t.Errorf("want '%d' got '%d', %v", want, got, numbers)
 	}
 }
+
+func TestSumSlice(t *testing.T) {
+
+	type params struct {
+		input []int
+		want  int
+	}
+
+	cases := []params{
+		{[]int{1, 2, 3}, 6},
+		{[]int{}, 0},
+		{[]int{1, 1, 1}, 3},
+		{[]int{1, 1, 2, 3, 1, 2, 3, 2, 3}, 18},
+		{[]int{1, 2, 3, 4, 5}, 15},
+	}
+
+	for _, x := range cases {
+		t.Run("test sum of slice", func(t *testing.T) {
+			got := SumSlice(x.input)
+			if got != x.want {
+				t.Errorf("want '%d' got '%d', %v", x.want, got, x.input)
+			}
+		})
+	}
+}
