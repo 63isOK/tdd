a73a466d1c30dd03beb058ec142141da1dda5b3c
63isOK <1876180681@qq.com>
2019-08-14 06:54:21 +0000

    添加另一种实现 调标准库api
    添加基准测试

--- loop/cloop.go
+++ loop/cloop.go
@@ -1,9 +1,22 @@
 package loop
 
+import (
+	"strings"
+)
+
 func repeat(char string, count int) (ret string) {
 	for i := 0; i < count; i++ {
 		ret += char
 	}
 
 	return
+}
+
+// RepeatWithStd repeat string with std lib
+func RepeatWithStd(char string, count int) string {
+	if count < 0 {
+		return ""
+	}
+
+	return strings.Repeat(char, count)
 }
--- loop/cloop_test.go
+++ loop/cloop_test.go
@@ -1,6 +1,7 @@
 package loop
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -27,8 +28,43 @@ func TestRepeat(t *testing.T) {
 	})
 }
 
+func TestRepeatWithStd(t *testing.T) {
+
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+
+		if got != want {
+			t.Errorf("want '%s' got '%s'", want, got)
+		}
+	}
+
+	t.Run("repeat 5 char", func(t *testing.T) {
+		got := repeat("c", 5)
+		want := "ccccc"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("repeat -1", func(t *testing.T) {
+		got := repeat("a", -1)
+		want := ""
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func ExampleRepeatWithStd() {
+	s := RepeatWithStd("abc", 4)
+	fmt.Println(s)
+	// Output:abcabcabcabc
+}
+
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		repeat("q", 3)
+		repeat("q", 300)
+	}
+}
+
+func BenchmarkRepeatWithStd(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		repeat("q", 300)
 	}
 }
