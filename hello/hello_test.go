42c106c21a736d0d4114fa2073d7c3a1e41be263
63isOK <1876180681@qq.com>
2019-08-14 01:15:32 +0000

    重构测试例子
    好处是提高测试例子的可读性
    t.Helper()指明函数是辅助函数,
    失败时的打印信息使用调用方的,不要使用辅助函数的行号

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -4,19 +4,22 @@ import "testing"
 
 func TestHelloWorld(t *testing.T) {
 
-	t.Run("say hello to someone", func(t *testing.T) {
-		got := HelloWorld("Jim")
-		want := "hello Jim"
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
 		if got != want {
 			t.Errorf("want '%s' got '%s'", want, got)
 		}
+	}
+
+	t.Run("say hello to someone", func(t *testing.T) {
+		got := HelloWorld("Jim")
+		want := "hello Jim"
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say hello world when there is nobody", func(t *testing.T) {
 		got := HelloWorld("")
 		want := "hello world"
-		if got != want {
-			t.Errorf("want '%s' got '%s'", want, got)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 }
