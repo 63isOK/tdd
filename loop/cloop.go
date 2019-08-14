077a8ad9b421bea07c0bb243730dc6b3e055942e
63isOK <1876180681@qq.com>
2019-08-14 06:14:58 +0000

    重复字符串 红灯
    添加了测试例子
    添加了最小能让测试例子跑起来的实现,测试测试结果是不通过

---
+++ loop/cloop.go
@@ -0,0 +1,5 @@
+package loop
+
+func repeat(char string, count int) string {
+	return ""
+}
---
+++ loop/cloop_test.go
@@ -0,0 +1,26 @@
+package loop
+
+import "testing"
+
+func TestRepeat(t *testing.T) {
+
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+
+		if got != want {
+			t.Errorf("want '%s' got '%s'", want, got)
+		}
+	}
+
+	t.Run("repeat 5 char", func(t *testing.T) {
+		got := repeat("c", 5)
+		want := "ccccc"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("repeat 7 char", func(t *testing.T) {
+		got := repeat("a", 7)
+		want := "aaaaaaa"
+		assertCorrectMessage(t, got, want)
+	})
+}
