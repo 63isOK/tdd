2851950d76fa320a9955777c6ee75178b595ba24
63isOK <1876180681@qq.com>
2019-08-15 08:59:23 +0000

    重构 增加测试失败时的打印信息 更加直观

--- p/p_test.go
+++ p/p_test.go
@@ -4,11 +4,11 @@ import (
 	"testing"
 )
 
-func assertCorrectMessage(t *testing.T, want, got float64) {
+func assertCorrectMessage(t *testing.T, want, got float64, shape Shape) {
 	t.Helper()
 
 	if int(want*100) != int(got*100) {
-		t.Errorf("want '%.2f' got '%.2f'", want, got)
+		t.Errorf("%#v want '%.2f' got '%.2f'", shape, want, got)
 	}
 }
 
@@ -17,21 +17,24 @@ func TestPerimeter(t *testing.T) {
 	checkPerimeter := func(t *testing.T, shape Shape, want float64) {
 		t.Helper()
 		got := shape.Perimeter()
-		assertCorrectMessage(t, want, got)
+		assertCorrectMessage(t, want, got, shape)
 	}
 
 	areaTest := []struct {
+		name  string
 		shape Shape
 		want  float64
 	}{
-		{shape: &Rectangle{Width: 3, Height: 4}, want: 14},
-		{shape: &Circle{Raduis: 10}, want: 62.8},
-		{shape: &Square{Width: 10}, want: 40},
-		{shape: &Square{Width: 3.32}, want: 13.28},
+		{name: "rectangle", shape: &Rectangle{Width: 3, Height: 4}, want: 14},
+		{name: "circle", shape: &Circle{Raduis: 10}, want: 62.8},
+		{name: "square", shape: &Square{Width: 10}, want: 40},
+		{name: "square", shape: &Square{Width: 3.32}, want: 13.28},
 	}
 
 	for _, tt := range areaTest {
-		checkPerimeter(t, tt.shape, tt.want)
+		t.Run(tt.name, func(t *testing.T) {
+			checkPerimeter(t, tt.shape, tt.want)
+		})
 	}
 }
 
@@ -40,20 +43,23 @@ func TestArea(t *testing.T) {
 	checkArea := func(t *testing.T, shape Shape, want float64) {
 		t.Helper()
 		got := shape.Area()
-		assertCorrectMessage(t, want, got)
+		assertCorrectMessage(t, want, got, shape)
 	}
 
 	areaTest := []struct {
+		name  string
 		shape Shape
 		want  float64
 	}{
-		{shape: &Rectangle{Width: 3, Height: 4}, want: 12},
-		{shape: &Circle{Raduis: 10}, want: 314},
-		{shape: &Square{Width: 10}, want: 100},
-		{shape: &Square{Width: 3.32}, want: 11.02},
+		{name: "rectangle", shape: &Rectangle{Width: 3, Height: 4}, want: 12},
+		{name: "circle", shape: &Circle{Raduis: 10}, want: 314},
+		{name: "square", shape: &Square{Width: 10}, want: 100},
+		{name: "square", shape: &Square{Width: 3.32}, want: 11.02},
 	}
 
 	for _, tt := range areaTest {
-		checkArea(t, tt.shape, tt.want)
+		t.Run(tt.name, func(t *testing.T) {
+			checkArea(t, tt.shape, tt.want)
+		})
 	}
 }
