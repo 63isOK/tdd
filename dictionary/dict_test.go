7ccdf1203d802bbfdead45d5697a85d190654677
63isOK <1876180681@qq.com>
2019-08-16 05:48:00 +0000

    map查找 重构
    重构测试例子,确保每个函数只干一件事

--- dictionary/dict_test.go
+++ dictionary/dict_test.go
@@ -2,12 +2,18 @@ package main
 
 import "testing"
 
+func assertStrings(t *testing.T, want, got string) {
+	t.Helper()
+
+	if want != got {
+		t.Errorf("want '%s' got '%s'", want, got)
+	}
+}
+
 func TestSearch(t *testing.T) {
 	d := map[string]string{"test": "there is a test case"}
 	got := Search(d, "test")
 	want := "there is a test case"
 
-	if got != want {
-		t.Errorf("want '%s' got '%s'", want, got)
-	}
+	assertStrings(t, want, got)
 }
