7d98a15d46d2ce35f60a8a2ac6d5fa730636d09b
63isOK <1876180681@qq.com>
2019-08-19 02:40:12 +0000

    使用通道来解决数据竞争

--- concurrency/check.go
+++ concurrency/check.go
@@ -1,21 +1,28 @@
 package concurrency
 
-import "time"
-
 // WebsiteChecker is check tool for website
 type WebsiteChecker func(string) bool
+
+type result struct {
+	string
+	bool
+}
 
 // CheckWebsites check urls
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
 
 	for _, url := range urls {
 		go func(u string) {
-			results[u] = wc(u)
+			resultChannel <- result{u, wc(u)}
 		}(url)
 	}
 
-	time.Sleep(2 * time.Second)
+	for i := 0; i < len(urls); i++ {
+		result := <-resultChannel
+		results[result.string] = result.bool
+	}
 
 	return results
 }
