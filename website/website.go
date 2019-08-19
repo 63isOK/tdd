6b82917900f63de9ce7470668cff7ff305ae33bc
63isOK <1876180681@qq.com>
2019-08-19 03:35:51 +0000

    绿灯

--- website/website.go
+++ website/website.go
@@ -1,5 +1,22 @@
 package website
 
-func racer(url1, url2 string) string {
-	return ""
+import (
+	"net/http"
+	"time"
+)
+
+func racer(urlA, urlB string) string {
+	startA := time.Now()
+	_, _ = http.Get(urlA)
+	costA := time.Since(startA)
+
+	startB := time.Now()
+	_, _ = http.Get(urlB)
+	costB := time.Since(startB)
+
+	if costA < costB {
+		return urlA
+	}
+
+	return urlB
 }
