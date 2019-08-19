2c5d6d00262ab7e37d968364069246881b09d5e7
63isOK <1876180681@qq.com>
2019-08-19 03:42:09 +0000

    使用mocking
    使用可靠测试代替不可靠测试

--- website/website_test.go
+++ website/website_test.go
@@ -1,15 +1,33 @@
 package website
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
-	url1 := "http://www.baidu.com"
-	url2 := "http://www.bing.com"
 
-	want := url1
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	url1 := slowServer.URL
+	url2 := fastServer.URL
+
+	want := url2
 	got := racer(url1, url2)
 
 	if want != got {
 		t.Errorf("want '%s', got '%s'", want, got)
 	}
+
+	slowServer.Close()
+	fastServer.Close()
 }
