4ad787bbf839e74f6a76761752f2c4b18c90303a
63isOK <1876180681@qq.com>
2019-08-22 06:20:58 +0000

    重构 将处理http请求的函数拆分,以便更高的可读性

--- http/player.go
+++ http/player.go
@@ -15,6 +15,7 @@ type StubPlayerStore struct {
 	scores map[string]int
 }
 
+// GetPlayerScore return the score of the player
 func (s *StubPlayerStore) GetPlayerScore(name string) int {
 	return s.scores[name]
 }
@@ -25,7 +26,24 @@ type PlayerServer struct {
 }
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		p.processSocre(w, r)
+	case http.MethodPost:
+		p.processWin(w)
+	}
+}
+
+func (p *PlayerServer) processSocre(w http.ResponseWriter, r *http.Request) {
 	player := r.URL.Path[len("/palyers/"):]
 
-	_, _ = fmt.Fprint(w, p.store.GetPlayerScore(player))
+	score := p.store.GetPlayerScore(player)
+
+	if score == 0 {
+		w.WriteHeader(http.StatusNotFound)
+	}
+	_, _ = fmt.Fprint(w, score)
+}
+func (p *PlayerServer) processWin(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusAccepted)
 }
--- http/player_test.go
+++ http/player_test.go
@@ -22,6 +22,7 @@ func TestServer(t *testing.T) {
 		response := httptest.NewRecorder()
 
 		server.ServeHTTP(response, request)
+		assertResponseCode(t, response.Code, http.StatusOK)
 		assertResponseBody(t, response.Body.String(), "20")
 	})
 
@@ -30,7 +31,24 @@ func TestServer(t *testing.T) {
 		response := httptest.NewRecorder()
 
 		server.ServeHTTP(response, request)
+		assertResponseCode(t, response.Code, http.StatusOK)
 		assertResponseBody(t, response.Body.String(), "10")
+	})
+
+	t.Run("return 404 when on missing players", func(t *testing.T) {
+		request := newScoreRequest("hugo")
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+		assertResponseCode(t, response.Code, http.StatusNotFound)
+	})
+
+	t.Run("accepted on post", func(t *testing.T) {
+		request := newStoreRequest("cobra")
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+		assertResponseCode(t, response.Code, http.StatusAccepted)
 	})
 }
 
@@ -39,9 +57,22 @@ func newScoreRequest(name string) *http.Request {
 	return request
 }
 
+func newStoreRequest(name string) *http.Request {
+	request, _ := http.NewRequest(http.MethodPost, fmt.Sprintf("/players/%s", name), nil)
+	return request
+}
+
 func assertResponseBody(t *testing.T, got, want string) {
 	t.Helper()
 	if want != got {
 		t.Errorf("want %s, got %s", want, got)
+	}
+}
+
+func assertResponseCode(t *testing.T, got, want int) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("want %d, got %d", want, got)
 	}
 }
